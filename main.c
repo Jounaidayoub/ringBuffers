441b947a9d09fde81e74dd38807a8bbebfccb6f4
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-11-08 15:42:22 +0100

    add basic ring buffer implementation

--- main.c
+++ main.c
@@ -1,6 +1,62 @@
+#include <stdlib.h>
 #include <stdio.h>
+#define MAX 10
+typedef struct RingBuffer
+{
+    int buffer[MAX];
+    int head;
+    int queue;
+} RingBuffer;
 
-int main(void) {
-    printf("Hello, World! \n");
+RingBuffer *init()
+{
+    RingBuffer *buffer = (RingBuffer *)malloc(sizeof(RingBuffer));
+    if (!buffer)
+    {
+        printf("cant allocate memeroy to the buffer :( \n closing ....");
+        exit(EXIT_FAILURE);
+    }
+
+    buffer->head = buffer->queue = 0;
+
+    return buffer;
+}
+RingBuffer *RB_add(RingBuffer *b, int value)
+{
+
+    b->buffer[b->queue] = value;
+    b->queue = (++b->queue) % MAX;
+    printf(" %d has been added\n",value);
+    return b;
+}
+
+int RB_remove(RingBuffer *b)
+{
+    if (b->head == b->queue)
+    {
+        printf("no element left to read !!");
+        return 0;
+    }
+
+    int element = b->buffer[b->head];
+    b->head = (++b->head) % MAX;
+    printf(" %d is removed \n",element);
+    return -1;
+}
+
+
+
+int main(void)
+{
+    RingBuffer *buffer = init();
+
+    for (int i = 0; i < 5; i++)
+    {
+        buffer = RB_add(buffer, i);
+    }
+
+    while(RB_remove(buffer));
+
+    printf("\nHello, World! \n");
     return 0;
 }
